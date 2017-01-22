701b627f24138a5505fd61e36e34d4f01c50e822
happyinspirit <amy@fuelingbrands.com>
2017-01-21 23:31:55 -0500

    small cleanups

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-global.$       = global.jQuery = require('jquery');
+global.$ = global.jQuery = require('jquery');
 var foundation = require('../../node_modules/foundation-sites/dist/js/foundation.js');
 
 $(document).foundation();
